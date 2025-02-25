46ab3a7d4264dd13a4fa1c03039155b78e7442a3
Leonard <leonardpueblos@gmail.com>
2025-02-25 22:21:48 +0800

    fixed fetch api

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,16 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.get('/api/data', (req, res) => {
+    const sampleData = {
+        message: 'connected'
+    };
+    res.json(sampleData);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
--- frontend/app.js
+++ frontend/app.js
@@ -9,4 +9,14 @@ function openFullImg(image) {
 function closeFullImg() {
     fullImgBox.style.display = "none";
 }
-  
+
+function fetchData() {
+    fetch('http://localhost:3000/api/data')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+fetchData();
